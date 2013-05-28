e611220835b8d55a2bdbbaf05c68b7f9ca7e0d2b
Jean-Baptiste Richardet <jeanbaptiste.richardet@gmail.com>
2013-05-28 17:39:16 +0300

    Update Lib.hx
    Haxe3 compatible

--- bootstrap/Lib.hx
+++ bootstrap/Lib.hx
@@ -3,15 +3,15 @@ package bootstrap;
 import haxe.macro.Expr;
 import haxe.macro.Context;
 
-@:macro class Lib {
+class Lib {
 	static var path : String = "lib/bootstrap";
 
-	public static function setPath( p : String ){
+	macro public static function setPath( p : String ){
 		path = p;
 		return Context.parse("{}", Context.currentPos() );
 	}
 
-	public static function includeFile( filename : String ){
+	macro public static function includeFile( filename : String ){
 		return Context.parse( "haxe.macro.Tools.includeFile(\"" + path + "/" + filename +"\")" , Context.currentPos() );
 	}
 
