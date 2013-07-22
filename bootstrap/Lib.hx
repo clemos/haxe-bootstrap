38fb9ac2ae9ed62b26af91d07528ec7c41a7c475
JbIPS <jeanbaptiste.richardet@gmail.com>
2013-07-22 13:14:59 +0200

    Fix Haxe3 compatibility

--- bootstrap/Lib.hx
+++ bootstrap/Lib.hx
@@ -3,15 +3,16 @@ package bootstrap;
 import haxe.macro.Expr;
 import haxe.macro.Context;
 
-class Lib {
+#if !haxe3 @:macro #end class Lib {
+
 	static var path : String = "lib/bootstrap";
 
-	macro public static function setPath( p : String ){
+	#if haxe3 macro #end public static function setPath( p : String ){
 		path = p;
 		return Context.parse("{}", Context.currentPos() );
 	}
 
-	public static function includeFile( filename : String ){
+	#if haxe3 macro #end public static function includeFile( filename : String ){
 #if haxe3	return Context.parse( "haxe.macro.Compiler.includeFile(\"" + path + "/" + filename +"\")" , Context.currentPos() );
 #else		return Context.parse( "haxe.macro.Tools.includeFile(\"" + path + "/" + filename +"\")" , Context.currentPos() ); #end
 	}
